4257be6fd1f4d636b5b418d6556175599547abeb
danielmedb <70508723+danielmedb@users.noreply.github.com>
2021-04-10 11:10:22 +0200

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -4,10 +4,9 @@
     {
         $values = $args;
         $apiKey = env("API_KEY");
-        $mostPopular = 'https://www.thecocktaildb.com/api/json/v2/' . $apiKey . '/' . $page . '';
+        $results = 'https://www.thecocktaildb.com/api/json/v2/' . $apiKey . '/' . $page . '';
 
-        $response = Http::get($mostPopular);
-        $mostPopular = $response->json();
+        $results = Http::get($results)->json();
 
-        return $mostPopular;
+        return $results;
     }
