284f876575a496174d2e7c7e73dfaf2752b7e184
danielmedb <70508723+danielmedb@users.noreply.github.com>
2021-04-13 11:46:39 +0200

    update

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -25,6 +25,13 @@ class RouteTest extends TestCase
          ->assertDatabaseHas('users', ['email' => 'Robot@test.se']);
     }
 
+    public function test_view_login_form()
+    {
+        $response = $this->get('/login');
+        $response->assertSuccessful();
+        $response->assertSeeText('Welcome back!');
+    }
+
     public function test_login_user()
     {
         $user = new User();
@@ -42,6 +49,33 @@ class RouteTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_login_failed()
+    {
+        $response = $this
+            ->followingRedirects()
+            ->post('login', [
+                'email' => 'invalid@invalid.com',
+                'password' => 'invalid',
+            ]);
+        $response->assertDontSeeText('Welcome');
+    }
+
+    public function test_logout_user()
+    {
+        $user = new User();
+        $user->username = 'Robot Test';
+        $user->email = 'Robot@test.se';
+        $user->password = Hash::make('password');
+        $user->save();
+
+        $response = $this
+        ->followingRedirects()
+        ->get('/logout');
+
+        $response->assertViewIs('auth.login');
+    }
+
 
     public function test_all_routes()
     {
@@ -88,4 +122,55 @@ class RouteTest extends TestCase
             }
         }
     }
+
+    public function test_search_for_drink_recipe()
+    {
+
+        $user = new User();
+        $user->username = 'Robot Test';
+        $user->email = 'Robot@test.se';
+        $user->password = Hash::make('password');
+        $user->save();
+
+        $response = $this
+        ->followingRedirects()
+        ->post('login', [
+            'email' => 'Robot@test.se',
+            'password' => 'password'
+        ]);
+
+        $response = $this
+        ->post('search', [
+            'search' => 'Vodka,gin',
+        ]);
+
+
+
+        if ($response->assertStatus(200)) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    // public function test_add_and_remove_recipe()
+    // {
+    //     $user = new User();
+    //     $user->username = 'Robot Test';
+    //     $user->email = 'Robot@test.se';
+    //     $user->password = Hash::make('password');
+    //     $user->save();
+
+    //     $response = $this
+    //     ->followingRedirects()
+    //     ->post('login', [
+    //         'email' => 'Robot@test.se',
+    //         'password' => 'password'
+    //     ]);
+
+    //     $response = $this
+    //     ->post('addFavorite');
+    //     $response->assertStatus(200);
+
+    // }
 }
